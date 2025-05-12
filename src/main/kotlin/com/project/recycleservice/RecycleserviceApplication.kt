cf2ed0d5195f7f6b33c8e025437747d34ef22ef9
jay <jieun.p974@gmail.com>
2025-05-12 22:06:33 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/project/recycleservice/RecycleserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.project.recycleservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecycleserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecycleserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/recycleservice/RecycleserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.recycleservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecycleserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
